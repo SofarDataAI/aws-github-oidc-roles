7b32d21f4209bd5c1c2abbc8abc681aef3c3596a
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-16 16:50:25 +0700

    Init project CDK

---
+++ test/aws-github-oidc-roles.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsGithubOidcRoles from '../lib/aws-github-oidc-roles-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-github-oidc-roles-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsGithubOidcRoles.AwsGithubOidcRolesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
